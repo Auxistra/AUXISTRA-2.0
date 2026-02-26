431bbe576e8c7a2eeeebb3fff79408967e07ec03
SashrikaVidanagama <sashrika.20240918@iit.ac.lk>
2026-02-26 13:39:05 +0530

    Add SearchBarWidget for search functionality

---
+++ lib/widgets/search_bar_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class SearchBarWidget extends StatelessWidget {
+
+  final String hint;
+
+  const SearchBarWidget({
+    super.key,
+    required this.hint,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 44,
+      decoration: BoxDecoration(
+        color: const Color(0xFF2C2C2E),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Row(
+        children: [
+
+          Icon(
+            Icons.search,
+            color: Colors.grey[400],
+          ),
+
+          const SizedBox(width: 8),
+
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: hint,
+                border: InputBorder.none,
+                hintStyle: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
